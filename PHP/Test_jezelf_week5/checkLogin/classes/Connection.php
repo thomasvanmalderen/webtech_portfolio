88c96a64df0bbc5b44eb6d0672d1030a700bd42b
Thomas Van malderen <thomas.vanmalderen@student.thomasmore.be>
2016-05-17 13:16:08 +0200

    updated webtech2project

--- PHP/Test_jezelf_week5/checkLogin/classes/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-		$sHost = "localhost";
-		$sUser = "root";
-		$sPassword = "root";
-		$sDatabase = "checkLogin";
-		$link = @mysqli_connect($sHost, $sUser, $sPassword, $sDatabase) or die("Database not found");
-
-?>
--- PHP/Test_jezelf_week5/iMessage/ajax/user.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    $name['name'] = $_SESSION['name'];
-    header('Content-Type: application/json');
-    echo json_encode($name);
-
-?>
